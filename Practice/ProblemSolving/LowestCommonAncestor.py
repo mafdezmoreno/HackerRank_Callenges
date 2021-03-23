85d4e2b19834f3783557fbb2a298cd5d807749d5
Miguel Angel <mafdezmoreno@outlook.es>
2021-03-23 23:35:36 +0100

    Trees Practice

---
+++ Practice/ProblemSolving/LowestCommonAncestor.py
@@ -0,0 +1,76 @@
+#https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor/problem
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+def lca(root, v1, v2):
+    #Enter your code here
+    if not root:
+        return None
+    if root.info == v1 or root.info ==v2:
+        return root
+    left = lca(root.left, v1, v2)
+    right = lca(root.right, v1, v2)
+    if right and left:
+        return root
+    return right or left
+
+
+tree = BinarySearchTree()
+#t = int(input())
+#arr = list(map(int, input().split()))
+with open('LowestCommonAncestor_1.txt') as file:
+    t = int(next(file))
+    arr = list(map(int, next(file).split()))
+    v = list(map(int, next(file).split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+#v = list(map(int, input().split()))
+ans = lca(tree.root, v[0], v[1])
+print (ans.info)
